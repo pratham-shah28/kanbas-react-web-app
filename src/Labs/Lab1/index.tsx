769460c14923948a9981d57f6b7933523bf0bb35
pratham-shah28 <psshah2803@gmail.com>
2024-09-12 14:22:50 -0400

    netlify fix 2

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -150,12 +150,12 @@ Your favorite books (in no particular order)
   <img id="wd-starship"
     width="400px"
    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-   alt="image"
+   alt="tasveer"
   />
   <br />
   Loading a local image:
   <br />
-  <img id="wd-teslabot" src="images/teslabot.jpeg" height="200px" alt="image"/>
+  <img id="wd-teslabot" src="images/teslabot.jpeg" height="200px" alt="tasveer"/>
 </div>
 <div id="wd-forms">
   <h4>Form Elements</h4>
