b4ea373096c0446f22e254f9a021f1cc2015fff4
pratham-shah28 <psshah2803@gmail.com>
2024-09-15 17:11:24 -0400

    third push lfg

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,22 @@
 // }
 
 // export default App;
-
-import React from "react";
+import Kanbas from "./Kanbas";
 import Labs from "./Labs";
+
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
+// export default App;
+
