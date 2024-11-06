dbf20446fab100bd3011404ae7cfa22ce6d02e49
pratham-shah28 <psshah2803@gmail.com>
2024-11-06 11:23:11 -0500

    a4 begins

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,12 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+export default function Lab4() {
+  console.log('Hello World!');
+  return(
+    <div id="wd-lab4">
+      <h3>Lab 4</h3>
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+    </div>
+  );
+}
