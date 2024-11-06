ea5ff5e076936bb6adc3c9e11f75ae0871dad029
pratham-shah28 <psshah2803@gmail.com>
2024-11-06 13:26:52 -0500

    till 2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,12 +1,33 @@
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 export default function Lab4() {
   console.log('Hello World!');
+  function sayHello() {
+    alert("Hello");
+  }
   return(
     <div id="wd-lab4">
       <h3>Lab 4</h3>
       <ClickEvent/>
       <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello}/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
     </div>
   );
 }
