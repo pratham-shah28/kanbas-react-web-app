99ad45a1bf6589a66fc3c2680ab8270ce6fc2525
pratham-shah28 <psshah2803@gmail.com>
2024-11-13 18:37:14 -0500

    the last stretch

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,15 @@
 // export default App;
 import Kanbas from "./Kanbas";
 import Labs from "./Labs";
+import store from "./Kanbas/store";
+import { Provider } from "react-redux";
 
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
+      <Provider store={store}>
+
       <div>
         
         <Routes>
@@ -39,6 +43,8 @@ export default function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
+      </Provider>
+
     </HashRouter>
   );
 }
