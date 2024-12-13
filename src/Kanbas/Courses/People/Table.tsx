3195d910b725519abb887726bf46b35237220a90
pratham-shah28 <psshah2803@gmail.com>
2024-12-12 23:42:20 -0500

    changes

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,38 +1,56 @@
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "react-router-dom";
-import * as db from "../../Database";
+import React from "react";
 import PeopleDetails from "./Details";
 import { Link } from "react-router-dom";
-export default function PeopleTable({ users = [] }: { users?: any[] }) {
+import { useSelector } from "react-redux";
 
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div id="wd-people-table">
       <PeopleDetails />
       <table className="table table-striped">
         <thead>
-          <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
         </thead>
         <tbody>
-  {users
-    .map((user: any) => (
-      <tr key={user._id}>
-        <td className="wd-full-name text-nowrap">
-        <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                {currentUser.role === "ADMIN" ? (
+                  <Link
+                    to={`/Kanbas/Account/Users/${user._id}`}
+                    className="text-decoration-none"
+                  >
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}</span>
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </Link>
+                ) : (
+                  <span>
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}</span>
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </span>
+                )}
+              </td>
 
-          <FaUserCircle className="me-2 fs-1 text-secondary" />
-          <span className="wd-first-name">{user.firstName}</span>
-          <span className="wd-last-name">{user.lastName}</span>
-          </Link>
-        </td>
-        <td className="wd-login-id">{user.loginId}</td>
-        <td className="wd-section">{user.section}</td>
-        <td className="wd-role">{user.role}</td>
-        <td className="wd-last-activity">{user.lastActivity}</td>
-        <td className="wd-total-activity">{user.totalActivity}</td>
-      </tr>
-    ))}
-</tbody>
-
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-    </div> );}
-
+    </div>
+  );
+}
